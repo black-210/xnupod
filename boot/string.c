dd2909ce4f5368a55484a608d8ee8342d9b7dec4
block980 <ibrahimmohamedalmona@gmail.com>
2026-09-25 06:40:01 +0300

    Fix i386 bootstrap build

--- boot/string.c
+++ boot/string.c
@@ -35,3 +35,14 @@ memset(void *ptr, int value, size_t n)
 
     return ptr;
 }
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char *d = (unsigned char *)dest;
+    const unsigned char *s = (const unsigned char *)src;
+
+    for (size_t i = 0; i < n; i++)
+        d[i] = s[i];
+
+    return dest;
+}
--- xnuxx.mk
+++ xnuxx.mk
@@ -1,33 +1,36 @@
 CC=clang
 LD=ld
 BUILD=build
-CFLAGS=-m32 -ffreestanding -fno-pic -fno-stack-protector -fno-builtin -fno-strict-aliasing -Wall -Wextra -Iinclude
+CFLAGS=--target=i386-unknown-elf -m32 -ffreestanding -fno-pic -fno-stack-protector -fno-builtin -fno-strict-aliasing -Wall -Wextra -Iinclude
 LDFLAGS=-m elf_i386 -T boot/linker.ld
 
 all: $(BUILD)/xnuxx.elf
 
 $(BUILD):
-mkdir -p $(BUILD)
+	mkdir -p $(BUILD)
 
 $(BUILD)/boot_entry.o: boot/boot_entry.c | $(BUILD)
-$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/boot_platform.o: boot/boot_platform.c | $(BUILD)
-$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/string.o: boot/string.c | $(BUILD)
-$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/kernel.o: kernel/kernel.c | $(BUILD)
-$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(BUILD)/bootloader.o: security/bootloader.c | $(BUILD)
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(BUILD)/multiboot2.o: boot/multiboot2.S | $(BUILD)
-$(CC) -m32 -x assembler-with-cpp -c $< -o $@
+	$(CC) --target=i386-unknown-elf -c $< -o $@
 
-$(BUILD)/xnuxx.elf: $(BUILD)/multiboot2.o $(BUILD)/boot_entry.o $(BUILD)/boot_platform.o $(BUILD)/string.o $(BUILD)/kernel.o
-$(LD) $(LDFLAGS) -o $@ $^
+$(BUILD)/xnuxx.elf: $(BUILD)/multiboot2.o $(BUILD)/boot_entry.o $(BUILD)/boot_platform.o $(BUILD)/string.o $(BUILD)/kernel.o $(BUILD)/bootloader.o
+	$(LD) $(LDFLAGS) -o $@ $^
 
 clean:
-rm -rf $(BUILD)
+	rm -rf $(BUILD)
 
 .PHONY: all clean
